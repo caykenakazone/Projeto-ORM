c70410a256f551f4ddfaa1826fc01a7d5b074c78
caykenakazone <carlos.nakazone@estudante.iftm.edu.br>
2023-03-14 21:25:04 -0300

    Alunos

---
+++ helloworld/src/main/java/com/iftm/api/helloworld/Aluno.java
@@ -0,0 +1,44 @@
+package com.iftm.api.helloworld;
+
+import java.util.Date;
+
+public class Aluno {
+    private String nome;
+    private int age;
+    private Date date = new Date();
+
+
+    // Construtor
+    public Aluno(String nome, int age) {
+        this.nome = nome;
+        this.age = age;
+    }
+    // Construtor
+    public Aluno(){
+
+    }
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
--- helloworld/src/main/java/com/iftm/api/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/iftm/api/helloworld/HelloworldApplication.java
@@ -2,13 +2,41 @@ package com.iftm.api.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
-
-		System.out.println("ola");
+	}
+	// Get
+	@GetMapping
+	public String helloWorld(){
+		return "Olá,Mundo!";
+	}
+	@GetMapping("/mars")
+	public String HelloMars(){
+		return "Olá, Marte!";
+	}
+	@GetMapping("/moon")
+	public String HelloMoon(){
+		return "Olá, Lua!";
+	}
+	@GetMapping("alunos")
+	public List<Aluno> getAluno(){
+		var listaDeAlunos = new ArrayList<Aluno>();
+		listaDeAlunos.add(new Aluno("Braulio",21));
+		listaDeAlunos.add(new Aluno("Rodrigo",30));
+		listaDeAlunos.add(new Aluno("Jose Pereira",81));
+		listaDeAlunos.add(new Aluno("Maria Laura",27));
+		return listaDeAlunos;
 	}
 }
